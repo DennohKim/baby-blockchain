410c6d26641df75c777073d182742a103bb9f07c
“dennohkim” <kimathidennis38@gmail.com>
2022-11-14 19:58:47 +0300

    create a chain

--- index.js
+++ index.js
@@ -49,10 +49,23 @@ class Block {
         const str = JSON.stringify(this); //stringify object
         const hash = crypto.createHash('SHA256'); //specify the hashing algorithm
         hash.update(str).end();
-        return hash.digest('hex');
+        return hash.digest('hex'); //a transaction has a link to the previous transaction
     }
 }
+//Linked list of blocks
 class Chain {
+    //Contructor to define genesis block
+    constructor() {
+        this.chain = [new Block(null, new Transaction(100, 'genesis', 'satoshi'))];
+    }
+    get lastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(transaction, senderPublicKey, signature) {
+        const newBlock = new Block(this.lastBlock.hash, transaction);
+        this.chain.push(newBlock); //add new block to chain
+    }
 }
+Chain.instance = new Chain(); //There should be only one blockchain therefore make it a singleton instance
 class Wallet {
 }
--- index.ts
+++ index.ts
@@ -31,13 +31,37 @@ class Block {
         const str = JSON.stringify(this); //stringify object
         const hash = crypto.createHash('SHA256'); //specify the hashing algorithm
         hash.update(str).end();
-        return hash.digest('hex');
+        return hash.digest('hex'); //a transaction has a link to the previous transaction
     }
 
 
 }
 
+//Linked list of blocks
 class Chain {
+    public static instance = new Chain(); //There should be only one blockchain therefore make it a singleton instance
+
+    chain: Block[];
+
+    //Contructor to define genesis block
+    constructor (){
+        this.chain = [new Block(null, new Transaction(100, 'genesis', 'satoshi'))];
+
+    }
+
+    get lastBlock() {
+        return this.chain[this.chain.length -  1];
+
+    }
+
+    addBlock(transaction: Transaction, senderPublicKey: string, signature: string){
+        const newBlock = new Block(this.lastBlock.hash, transaction); 
+        this.chain.push(newBlock); //add new block to chain
+
+    }
+
+    
+
 
 }
 
